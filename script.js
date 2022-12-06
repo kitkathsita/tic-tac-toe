65d02a0530c80c7b8d55f9691caa2c5cf8f6911a
kitkath <katherinpgr_99@hotmail.com>
2022-12-06 18:52:47 -0500

    new features added, now it shows whose player's turn and who's the winner

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const spaces = document.querySelectorAll('.space')
 const playAgainButton = document.getElementById('playagain')
+const playerOne = document.querySelector('.one')
+const playerTwo = document.querySelector('.two')
+const winner = document.querySelector('.winner')
 
 const gameboard = (() => {
   let game = ['', '', '', '', '', '', '', '', '']
@@ -10,10 +13,14 @@ const gameboard = (() => {
       if (value === -1) {
         this.innerHTML = 'x'
         game[this.getAttribute('position')] = 'x'
+        playerTwo.classList.add('active')
+        playerOne.classList.remove('active')
         value = 1
       } else {
         this.innerHTML = 'o'
         game[this.getAttribute('position')] = 'o'
+        playerOne.classList.add('active')
+        playerTwo.classList.remove('active')
         value = -1
       }
     }
@@ -23,10 +30,34 @@ const gameboard = (() => {
   function checkWinner(game) {
     console.log(game)
     if (game[0]===game[3] && game[3]===game[6] && game[0] != '' || game[1]===game[4] && game[4]===game[7] && game[1] != ''|| game[2]===game[5]&& game[5]===game[8] && game[2] != '') {
+      playerOne.classList.remove('active')
+      playerTwo.classList.remove('active')
+      if (game[0]==='o' || game[1]==='o' || game[2]==='o') {
+        winner.innerHTML = 'WINNER: PLAYER 2'
+      } else {
+        winner.innerHTML = 'WINNER: PLAYER 1'
+      }
+      winner.classList.remove('now')
       gameover()
     } else if (game[0]===game[1] && game[1]===game[2] && game[0] != '' || game[3]===game[4] && game[4]===game[5] && game[3] != '' || game[6]===game[7] && game[7]===game[8] && game[6] != '') {
+      playerOne.classList.remove('active')
+      playerTwo.classList.remove('active')
+      if (game[0]==='o' || game[3]==='o' || game[6]==='o') {
+        winner.innerHTML = 'WINNER: PLAYER 2'
+      } else {
+        winner.innerHTML = 'WINNER: PLAYER 1'
+      }
+      winner.classList.remove('now')
       gameover()
     } else if (game[0]===game[4] && game[4]===game[8] && game[0] != ''|| game[2]===game[4] && game[4]===game[6] && game[2] != '') {
+      playerOne.classList.remove('active')
+      playerTwo.classList.remove('active')
+      if (game[0]==='o' || game[2]==='o') {
+        winner.innerHTML = 'WINNER: PLAYER 2'
+      } else {
+        winner.innerHTML = 'WINNER: PLAYER 1'
+      }
+      winner.classList.remove('now')
       gameover()
     }
   }
@@ -37,6 +68,7 @@ const gameboard = (() => {
     spaces.forEach(spaces => spaces.innerHTML = '')
     spaces.forEach(spaces => spaces.addEventListener('click', gameboard.gameBoard))
     playAgainButton.classList.add('hide')
+    winner.classList.add('now')
   }
 
   return {
